edd83324a8fae9a3e6b137bd1f3b5ccd9d5e56e2
Arijit Paine <99323868+codewithap@users.noreply.github.com>
2023-07-15 08:31:20 +0530

    Add files via upload

--- static/js/epis.js
+++ static/js/epis.js
@@ -116,64 +116,6 @@ function loadEp(gogoid,poster){
         file1 = jsonResponse['source'][0]['file'];
         file2 = jsonResponse['source_bk'][0]['file'];
         
-        let html = `<div data-shaka-player-container style="width: 100%; height:100%"
-        data-shaka-player-cast-receiver-id="8D8C71A7">
-      <!-- The data-shaka-player tag will make the UI library use this video element.
-           If no video is provided, the UI will automatically make one inside the container div. -->
-     <video autoplay poster="${poster}" data-shaka-player id="video" style="width:100%;height:100%" src="${file1}}"></video>
-   </div>`;
-        document.querySelector(".video").innerHTML = html;
-        let manifestUri = file1
-        async function init() {
-          // When using the UI, the player is made automatically by the UI object.
-          const video = document.getElementById('video');
-          const ui = video['ui'];
-          const controls = ui.getControls();
-          const player = controls.getPlayer();
-          window.player = player;
-          window.ui = ui;
-        
-          // Listen for error events.
-          player.addEventListener('error', onPlayerErrorEvent);
-          controls.addEventListener('error', onUIErrorEvent);
-        
-          // Try to load a manifest.
-          // This is an asynchronous process.
-          try {
-            await player.load(manifestUri);
-            // This runs if the asynchronous load is successful.
-            console.log('The video has now been loaded!');
-          } catch (error) {
-            onPlayerError(error);
-          }
-        }
-        
-        function onPlayerErrorEvent(errorEvent) {
-          // Extract the shaka.util.Error object from the event.
-          onPlayerError(event.detail);
-        }
-        
-        function onPlayerError(error) {
-          // Handle player error
-          console.error('Error code', error.code, 'object', error);
-        }
-        
-        function onUIErrorEvent(errorEvent) {
-          // Extract the shaka.util.Error object from the event.
-          onPlayerError(event.detail);
-        }
-        
-        function initFailed(errorEvent) {
-          // Handle the failure to load; errorEvent.detail.reasonCode has a
-          // shaka.ui.FailReasonCode describing why.
-          console.error('Unable to load the UI library!');
-        }
-        
-        // Listen to the custom shaka-ui-loaded event, to wait until the UI is loaded.
-        document.addEventListener('shaka-ui-loaded', init);
-        // Listen to the custom shaka-ui-load-failed event, in case Shaka Player fails
-        // to load (e.g. due to lack of browser support).
-        document.addEventListener('shaka-ui-load-failed', initFailed);
         console.log(file1)
         console.log(poster)
         console.log(file2)

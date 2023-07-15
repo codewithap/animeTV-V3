6ea0b138bb60afbc2f0143857b461a89527ae5eb
Arijit Paine <99323868+codewithap@users.noreply.github.com>
2023-07-15 08:25:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("main.html")
 
+@app.route("/anime/<id>")
+def anime(id):
+    return render_template("main.html", mal_id = id)
+
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=80)
+    app.run(debug=True,host="0.0.0.0")

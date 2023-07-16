1f0454208244e80ef70f0e4891d2d0a7725afd49
Arijit Paine <99323868+codewithap@users.noreply.github.com>
2023-07-16 10:02:15 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ def home():
 @app.route("/anime/<id>")
 def anime(id):
     return render_template("main.html", mal_id = id)
+
+@app.route("/play")
+def play():
+    m3u8 = request.args.get("m3u8")
+    m3u8_2 = request.args.get("m3u8_2")
+    poster = request.args.get("poster")
+    return render_template("player.html", m3u8 = m3u8, m3u8_2 = m3u8_2, poster = poster)
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
